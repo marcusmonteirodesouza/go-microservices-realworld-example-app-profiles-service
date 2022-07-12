ed692ca372f30cbb53e1ec783a2e4b9edd62903a
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-11 20:15:11 -0400

    Refactor tests a little bit

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 ./start.sh "$1"
 go clean -testcache
-BASE_URL=http://localhost:8080 USERS_SERVICE_BASE_URL=http://localhost:8081 go test -v ./... && USERS_SERVICE_IMAGE_URL="$1" docker compose down
+BASE_URL=http://localhost:8080 USERS_SERVICE_BASE_URL=http://localhost:8081 go test -v ./... && USERS_SERVICE_IMAGE="$1" docker compose down
